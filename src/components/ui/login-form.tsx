540568c54971a06e63c2da25eb56bd0c155dda45
Pushpal Patil <gauripushp@gmail.com>
2025-06-13 16:21:25 -0400

    finally done w the tutorial

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -20,15 +20,16 @@ import {
   FormLabel,
   FormMessage
 } from "@/components/ui/form";
-import { signIn } from "@/server/users";
 import { zodResolver } from "@hookform/resolvers/zod"; // add zodresolver
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod"; // add z
 //import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import { signIn } from "@/server/users";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation"; //add useRouter
 import { useState } from "react";
-import { Loader2 } from "lucide-react";
 
 
 const formSchema = z.object({
@@ -56,6 +57,14 @@ export function LoginForm({
       password: "",
     },
   });
+
+  // 5. add signInWIthGoogleHandler
+  const signInWIthGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+    });
+  }
 
   // 2. Define a submit handler.
 
@@ -89,7 +98,8 @@ export function LoginForm({
               {/* <form> REPLACE THIS FORM FIELD W THE ONE IN THE SHADECN TUTORIAL*/}
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
-                  <Button variant="outline" className="w-full">
+
+                  <Button variant="outline" className="w-full" type="button" onClick={signInWIthGoogle}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path
                         d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -98,6 +108,7 @@ export function LoginForm({
                     </svg>
                     Login with Google
                   </Button>
+
                 </div>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                   <span className="bg-card text-muted-foreground relative z-10 px-2">
@@ -146,18 +157,18 @@ export function LoginForm({
                       </Link>
                     </div>
                   </div>
-                  <Button type="submit" className="w-full" disabled = {isLoading}>
+                  <Button type="submit" className="w-full" disabled={isLoading}>
                     {/* while it's loading, add a circle thingy and loading thingy  */}
-                    
-                    {isLoading ? <Loader2 className="size-4 animate-spin"/> : "Login"}
+
+                    {isLoading ? <Loader2 className="size-4 animate-spin" /> : "Login"}
 
                   </Button>
                 </div>
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
-                  <a href="#" className="underline underline-offset-4">
+                  <Link href="#" className="underline underline-offset-4">
                     Sign up
-                  </a>
+                  </Link>
                 </div>
               </div>
 
